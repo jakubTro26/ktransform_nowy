6060a1f24e482e3fc3c9b9aef2b650b29d7f4ff1
kuba <info@weblead.pl>
2021-06-01 13:09:56 +0200

    po

--- wp-content/themes/ievent/single-speakers.php
+++ wp-content/themes/ievent/single-speakers.php
@@ -1,7 +1,202 @@
 
 
 
-<head class="test"></head>
+<head>  
+	      
+          <!-- Basic Page Needs
+        ================================================== -->
+          <meta charset="UTF-8">
+             <meta name="description" content="30.06.2021 Warszawa">
+          <meta name="author" content="">
+          <link rel="profile" href="https://gmpg.org/xfn/11">
+          <link rel="pingback" href="https://kongrestransformacji.pl/xmlrpc.php">
+      
+          <link rel="stylesheet" type="text/css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/mojstyl.css?ver=901">
+          <!-- Mobile Specific Metas
+        ================================================== -->
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">    
+          <link rel="shortcut icon" href="https://kongrestransformacji.pl/wp-content/themes/ievent/images/favicon.ico">    
+          <link rel="apple-touch-icon" href="https://kongrestransformacji.pl/wp-content/themes/ievent/images/apple-touch-icon-57x57.png">    
+          <link rel="apple-touch-icon" sizes="114x114" href="https://kongrestransformacji.pl/wp-content/themes/ievent/images/apple-touch-icon-114x114.png">   
+          <link rel="apple-touch-icon" sizes="72x72" href="https://kongrestransformacji.pl/wp-content/themes/ievent/images/apple-touch-icon-72x72.png">    
+          <link rel="apple-touch-icon" sizes="144x144" href="https://kongrestransformacji.pl/wp-content/themes/ievent/images/apple-touch-icon-144x144.png">	<!-- CSS
+        ================================================== -->
+          
+          <!-- Web Fonts  -->
+          <!--Google Web Font Loader-->
+                     
+                      
+               
+               
+                      
+          
+                     
+          <script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/sG0iO6gHcGdWJzjJjW9AY49S/recaptcha__pl.js" crossorigin="anonymous" integrity="sha384-Q0jQmroDHgqMVTx1DEOagZqRv6023o5d78MZ6ENpHvR8bXRLs2vAQJydtPx2uy8z"></script><script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/sG0iO6gHcGdWJzjJjW9AY49S/recaptcha__pl.js" crossorigin="anonymous" integrity="sha384-Q0jQmroDHgqMVTx1DEOagZqRv6023o5d78MZ6ENpHvR8bXRLs2vAQJydtPx2uy8z"></script><script src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" type="text/javascript" async=""></script><script type="text/javascript">
+          WebFontConfig = {
+              google: { 
+              families:[ "Open+Sans:400,400italic,700,700italic:latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese", "Roboto:400,400italic,700,700italic:latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese", "Oswald:400,400italic,700,700italic:latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese" ]},
+                      
+          };
+          (function() {
+              var wf = document.createElement('script');
+              wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+              wf.type = 'text/javascript';
+              wf.async = 'true';
+              var s = document.getElementsByTagName('script')[0];
+              s.parentNode.insertBefore(wf, s);
+          })();
+          </script>   
+         
+         
+          <!--[if IE]>
+              <link rel="stylesheet" href="css/ie.css">
+          <![endif]-->
+          
+          <!--[if lte IE 8]>
+              <script src="vendor/respond.js"></script>
+          <![endif]-->
+          
+          <!--[if lt IE 9]>
+              <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+          <![endif]-->
+          
+          
+          <title>Kontakt – kongrestransformacji.pl</title>
+      <link rel="dns-prefetch" href="//www.google.com">
+      <link rel="dns-prefetch" href="//s.w.org">
+      <link rel="alternate" type="application/rss+xml" title="kongrestransformacji.pl » Kanał z wpisami" href="https://kongrestransformacji.pl/feed/">
+      <link rel="alternate" type="application/rss+xml" title="kongrestransformacji.pl » Kanał z komentarzami" href="https://kongrestransformacji.pl/comments/feed/">
+              <script type="text/javascript">
+                  window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.0\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.0\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/kongrestransformacji.pl\/wp-includes\/js\/wp-emoji-release.min.js"}};
+                  !function(e,a,t){var n,r,o,i=a.createElement("canvas"),p=i.getContext&&i.getContext("2d");function s(e,t){var a=String.fromCharCode;p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,e),0,0);e=i.toDataURL();return p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,t),0,0),e===i.toDataURL()}function c(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(o=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},r=0;r<o.length;r++)t.supports[o[r]]=function(e){if(!p||!p.fillText)return!1;switch(p.textBaseline="top",p.font="600 32px Arial",e){case"flag":return s([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])?!1:!s([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!s([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]);case"emoji":return!s([55357,56424,8205,55356,57212],[55357,56424,8203,55356,57212])}return!1}(o[r]),t.supports.everything=t.supports.everything&&t.supports[o[r]],"flag"!==o[r]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[o[r]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(n=t.source||{}).concatemoji?c(n.concatemoji):n.wpemoji&&n.twemoji&&(c(n.twemoji),c(n.wpemoji)))}(window,document,window._wpemojiSettings);
+              </script><script src="https://kongrestransformacji.pl/wp-includes/js/wp-emoji-release.min.js" type="text/javascript" defer=""></script>
+              <style type="text/css">
+      img.wp-smiley,
+      img.emoji {
+          display: inline !important;
+          border: none !important;
+          box-shadow: none !important;
+          height: 1em !important;
+          width: 1em !important;
+          margin: 0 .07em !important;
+          vertical-align: -0.1em !important;
+          background: none !important;
+          padding: 0 !important;
+      }
+      </style>
+          <link rel="stylesheet" id="dashicons-css" href="https://kongrestransformacji.pl/wp-includes/css/dashicons.min.css" type="text/css" media="all">
+      <link rel="stylesheet" id="admin-bar-css" href="https://kongrestransformacji.pl/wp-includes/css/admin-bar.min.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-skeleton-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/skeleton.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-dynamic-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/dynamic_ievent.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-font-awesome-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/fonts/font-awesome.min.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-theme-animate-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/theme-animate.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-theme-elements-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/theme-elements.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-theme-responsive-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/theme-responsive.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-plugins-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/plugins.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-magnific-popup-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/vendor/magnific-popup/magnific-popup.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-flexslider-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/vendor/flexslider/flexslider.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-prettyPhoto-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/vendor/prettyPhoto/prettyPhoto.css" type="text/css" media="all">
+      <link rel="stylesheet" id="ievent-vc-style-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/css/vc_style.css" type="text/css" media="all">
+      <link rel="stylesheet" id="stylesheet-css" href="https://kongrestransformacji.pl/wp-content/themes/ievent/style.css" type="text/css" media="all">
+      <link rel="stylesheet" id="wp-block-library-css" href="https://kongrestransformacji.pl/wp-includes/css/dist/block-library/style.min.css" type="text/css" media="all">
+      <link rel="stylesheet" id="contact-form-7-css" href="https://kongrestransformacji.pl/wp-content/plugins/contact-form-7/includes/css/styles.css" type="text/css" media="all">
+      <link rel="stylesheet" id="rs-plugin-settings-css" href="https://kongrestransformacji.pl/wp-content/plugins/revslider/public/assets/css/settings.css" type="text/css" media="all">
+      <style id="rs-plugin-settings-inline-css" type="text/css">
+      #rs-demo-id {}
+      </style>
+      <link rel="stylesheet" id="wordfenceAJAXcss-css" href="https://kongrestransformacji.pl/wp-content/plugins/wordfence/css/wordfenceBox.1603293147.css" type="text/css" media="all">
+      <link rel="stylesheet" id="wp-mail-smtp-admin-bar-css" href="https://kongrestransformacji.pl/wp-content/plugins/wp-mail-smtp/assets/css/admin-bar.min.css" type="text/css" media="all">
+      <link rel="stylesheet" id="duplicate-post-css" href="https://kongrestransformacji.pl/wp-content/plugins/duplicate-post/duplicate-post.css" type="text/css" media="all">
+      <link rel="stylesheet" id="js_composer_front-css" href="https://kongrestransformacji.pl/wp-content/plugins/js-composer/assets/css/js_composer.min.css" type="text/css" media="all">
+      <!--[if IE 8]>
+      <link rel='stylesheet' id='ie8-css'  href='https://kongrestransformacji.pl/wp-content/themes/ievent/css/ie.css' type='text/css' media='all' />
+      <![endif]-->
+      <!--[if IE 7]>
+      <link rel='stylesheet' id='ie7-css'  href='https://kongrestransformacji.pl/wp-content/themes/ievent/css/ie-7.css' type='text/css' media='all' />
+      <![endif]-->
+      <!--[if lt IE 7]>
+      <link rel='stylesheet' id='ie6-css'  href='https://kongrestransformacji.pl/wp-content/themes/ievent/css/ie-6.css' type='text/css' media='all' />
+      <![endif]-->
+      <link rel="stylesheet" id="wp-add-custom-css-css" href="https://kongrestransformacji.pl/" type="text/css" media="all">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic%7CRoboto:400,400italic,700,700italic%7COswald:400,400italic,700,700italic&amp;subset=latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese,latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese,latin,greek-ext,cyrillic,latin-ext,greek,cyrillic-ext,vietnamese" media="all"><script type="text/javascript" id="jquery-core-js-extra">
+      /* <![CDATA[ */
+      var ajaxVars = {"ajaxurl":"https:\/\/kongrestransformacji.pl\/wp-admin\/admin-ajax.php","ajax_nonce":"80180c6ffd"};
+      /* ]]> */
+      </script>
+      <script type="text/javascript" src="https://kongrestransformacji.pl/wp-includes/js/jquery/jquery.js" id="jquery-core-js"></script>
+      <script type="text/javascript" src="https://kongrestransformacji.pl/wp-content/plugins/revslider/public/assets/js/jquery.themepunch.tools.min.js" id="tp-tools-js"></script>
+      <script type="text/javascript" src="https://kongrestransformacji.pl/wp-content/plugins/revslider/public/assets/js/jquery.themepunch.revolution.min.js" id="revmin-js"></script>
+      <script type="text/javascript" id="wordfenceAJAXjs-js-extra">
+      /* <![CDATA[ */
+      var WFAJAXWatcherVars = {"nonce":"0800e91952"};
+      /* ]]> */
+      </script>
+      <script type="text/javascript" src="https://kongrestransformacji.pl/wp-content/plugins/wordfence/js/admin.ajaxWatcher.1603293147.js" id="wordfenceAJAXjs-js"></script>
+      <script type="text/javascript" src="https://kongrestransformacji.pl/wp-content/themes/ievent/vendor/modernizr.js" id="ievent-modernizr-js"></script>
+      <script type="text/javascript" src="https://www.google.com/recaptcha/api.js" id="ievent-google-recaptcha-js"></script>
+      <link rel="https://api.w.org/" href="https://kongrestransformacji.pl/wp-json/"><link rel="alternate" type="application/json" href="https://kongrestransformacji.pl/wp-json/wp/v2/pages/2032"><link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://kongrestransformacji.pl/xmlrpc.php?rsd">
+      <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://kongrestransformacji.pl/wp-includes/wlwmanifest.xml"> 
+      <meta name="generator" content="WordPress 5.5.5">
+      <link rel="canonical" href="https://kongrestransformacji.pl/kontakt/">
+      <link rel="shortlink" href="https://kongrestransformacji.pl/?p=2032">
+      <link rel="alternate" type="application/json+oembed" href="https://kongrestransformacji.pl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fkongrestransformacji.pl%2Fkontakt%2F">
+      <link rel="alternate" type="text/xml+oembed" href="https://kongrestransformacji.pl/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fkongrestransformacji.pl%2Fkontakt%2F&amp;format=xml">
+              <script type="text/javascript">
+              var ajaxurl = 'https://kongrestransformacji.pl/wp-admin/admin-ajax.php';
+              </script>
+              <meta name="generator" content="Powered by WPBakery Page Builder - drag and drop page builder for WordPress.">
+      <!--[if lte IE 9]><link rel="stylesheet" type="text/css" href="https://kongrestransformacji.pl/wp-content/plugins/js-composer/assets/css/vc_lte_ie9.min.css" media="screen"><![endif]--><style type="text/css" media="print">#wpadminbar { display:none; }</style>
+          <style type="text/css" media="screen">
+          html { margin-top: 32px !important; }
+          * html body { margin-top: 32px !important; }
+          @media screen and ( max-width: 782px ) {
+              html { margin-top: 46px !important; }
+              * html body { margin-top: 46px !important; }
+          }
+      </style>
+          <meta name="generator" content="Powered by Slider Revolution 5.4.6.4 - responsive, Mobile-Friendly Slider Plugin for WordPress with comfortable drag and drop interface.">
+      <script type="text/javascript">function setREVStartSize(e){
+                      try{ var i=jQuery(window).width(),t=9999,r=0,n=0,l=0,f=0,s=0,h=0;					
+                          if(e.responsiveLevels&&(jQuery.each(e.responsiveLevels,function(e,f){f>i&&(t=r=f,l=e),i>f&&f>r&&(r=f,n=e)}),t>r&&(l=n)),f=e.gridheight[l]||e.gridheight[0]||e.gridheight,s=e.gridwidth[l]||e.gridwidth[0]||e.gridwidth,h=i/s,h=h>1?1:h,f=Math.round(h*f),"fullscreen"==e.sliderLayout){var u=(e.c.width(),jQuery(window).height());if(void 0!=e.fullScreenOffsetContainer){var c=e.fullScreenOffsetContainer.split(",");if (c) jQuery.each(c,function(e,i){u=jQuery(i).length>0?u-jQuery(i).outerHeight(!0):u}),e.fullScreenOffset.split("%").length>1&&void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0?u-=jQuery(window).height()*parseInt(e.fullScreenOffset,0)/100:void 0!=e.fullScreenOffset&&e.fullScreenOffset.length>0&&(u-=parseInt(e.fullScreenOffset,0))}f=u}else void 0!=e.minHeight&&f<e.minHeight&&(f=e.minHeight);e.c.closest(".rev_slider_wrapper").css({height:f})					
+                      }catch(d){console.log("Failure at Presize of Slider:"+d)}
+                  };</script>
+      <script type="text/javascript">
+       
+          
+      </script>
+          
+      <style type="text/css">
+              body{ font-family: Open Sans, Arial, Helvetica, sans-serif ;}
+                      h1,h2,h3,h4,h5,h6,h7{ font-family: Open Sans, Arial, Helvetica, sans-serif !important;}
+              div.subtitle{ font-family: Open Sans, Arial, Helvetica, sans-serif !important;}
+                      nav ul li a{ font-family: Roboto, Arial, Helvetica, sans-serif !important;}
+                      footer h1,footer h2,footer h3,footer h4,footer h5,footer h6,{ font-family: Open Sans, Arial, Helvetica, sans-serif !important;}
+                  
+                  
+          
+                  body{font-size: 14px; font-weight: normal; color: ; }
+                  h1{ font-size: 32px; font-weight: normal; color: ; line-height: 30px;font-weight: 300;}
+                  h2{ font-size: 23px; font-weight: normal; color: ; }
+                  h3{ font-size: 18px; font-weight: normal; color: ; }
+                  h4{ font-size: 16px; font-weight: normal; color: ; }
+                  h5{ font-size: 15px; font-weight: normal; color: ; }
+                  h6{ font-size: 14px; font-weight: normal; color: ; }
+                  h1 a, h2 a, h3 a, h4 a, h5 a, h6 a, h1 a:visited, h2 a:visited, h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited  { font-weight: inherit; color: inherit; }
+      body{
+          }
+      
+      
+      </style>
+      <style type="text/css" data-type="vc_shortcodes-custom-css">.vc_custom_1544716060121{padding-top: 0px !important;}.vc_custom_1544716064097{margin-bottom: 4.2rem !important;}</style><noscript><style type="text/css"> .wpb_animate_when_almost_visible { opacity: 1; }</style></noscript>    
+          
+          <style>
+              </style>
+              
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
+      
+      
+      </head>
 <?php 
 global $ievent_data;
 
