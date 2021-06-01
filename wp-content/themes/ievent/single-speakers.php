7aafaac0254fe06bed0109a63f8b3bda7f705bd6
kuba <info@weblead.pl>
2021-06-01 13:06:20 +0200

    po

--- wp-content/themes/ievent/single-speakers.php
+++ wp-content/themes/ievent/single-speakers.php
@@ -16,7 +16,18 @@ global $ievent_data;
         <div id="primary" class="content-area">
             <div class="container">
                 <div class="sixteen columns jx-ievent-padding f <?php echo $class_single; ?>">
-                
+                <div id="home" class="jx-ievent-page-titlebar">
+                    <div class="page-titlebar-bg parallax-no" style="background: url(&quot;https://kongresksiegowych.pl/wp-content/uploads/2018/04/kamil-gliwinski-568269-unsplash.jpg&quot;) center center;"></div>
+                    <!-- Background Image -->                    
+                    <div class="container">
+                                                <div class="jx-ievent-page-titlebar-items">
+                            <div class="sixteen columns left">
+                                <div class="jx-ievent-breadcrumb"><a href="https://kongrestransformacji.pl/" rel="v:url" property="v:title">Home</a><span class="current">Kontakt</span></div>
+                            </div>  
+                            <!-- Page Title-->                           
+                        </div>
+                    	                    </div>
+                </div>
                 
                 	<?php while ( have_posts() ) : the_post(); ?>
             
