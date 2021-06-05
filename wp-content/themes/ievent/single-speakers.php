cdb59ae1c606b6994cc237825410df74b3404713
kuba <info@weblead.pl>
2021-06-05 17:05:28 +0200

    pro

--- wp-content/themes/ievent/single-speakers.php
+++ wp-content/themes/ievent/single-speakers.php
@@ -217,7 +217,7 @@ global $ievent_data;
             <div class="container">
                 <div class="sixteen columns jx-ievent-padding f <?php echo $class_single; ?>">
                 <div id="home" class="jx-ievent-page-titlebar">
-                    <div class="page-titlebar-bg parallax-no" style="background: url(&quot;https://kongresksiegowych.pl/wp-content/uploads/2018/04/kamil-gliwinski-568269-unsplash.jpg&quot;) center center;"></div>
+                    <div class="page-titlebar-bg a parallax-no" style="background: url(&quot;https://kongresksiegowych.pl/wp-content/uploads/2018/04/kamil-gliwinski-568269-unsplash.jpg&quot;) center center;"></div>
                     <!-- Background Image -->                    
                     <div class="container">
                                                 <div class="jx-ievent-page-titlebar-items">
