8de062e4d60dafcb92365c8d56d22a8172ae3481
kuba <info@weblead.pl>
2021-06-09 14:12:33 +0200

    pro

--- mainscript.js
+++ mainscript.js
@@ -20,6 +20,12 @@ $('#ChildTab-1 > div > div > div > div:nth-child(5)').style.display="none";
 
 }
 
+if(window.location.href=="https://kongrestransformacji.pl/partnerzy/")
+{
+$('.jx-ievent-bar-head').innerText="535-312-144";
+  
+}
+
 if(window.location.href=="https://kongrestransformacji.pl/kontakt/")
 {
 
