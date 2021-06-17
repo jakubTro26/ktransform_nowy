89fb8205d69f192107118915eb927210f995c244
kuba <info@weblead.pl>
2021-06-17 12:20:15 +0200

    pro

--- mainscript.js
+++ mainscript.js
@@ -61,8 +61,7 @@ $('#sponsorzy').style.width="400px";
 $('#sponsorzy').style.position="relative";
 $('#sponsorzy').style.left="50%";
 $('#sponsorzy').style.transform="translateX(-44%)";
-
-debugger; dodajimg1();
+ dodajimg1();
   dodajimg2();
 
   $('#sponsors > div > div > div > div > div:nth-child(2) > div > div > div > div:nth-child(3)').style.height="70px";
