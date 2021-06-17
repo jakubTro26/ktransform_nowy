002369f4d12e43031269bad719c4d44dd14e7394
kuba <info@weblead.pl>
2021-06-17 12:36:30 +0200

    pro

--- mainscript.js
+++ mainscript.js
@@ -183,7 +183,7 @@ var maindiv = document.querySelector('#sponsors > div > div > div > div > div:nt
 
 
 wspimg = function(){
-  var b = document.createElement('a');
+ debugger; var b = document.createElement('a');
   b.style.padding="20px";
   b.href="https://kongrestransformacji.pl/wp-content/uploads/2021/05/image-21.png";
   var bimg=document.createElement('img');
