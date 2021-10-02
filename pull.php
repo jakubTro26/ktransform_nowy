eb0ada1dab6534da5ee65de5a3cec546b0c403b6
kuba <info@weblead.pl>
2021-10-02 14:15:01 +0200

    first

--- pull.php
+++ pull.php
@@ -6,7 +6,7 @@
 
 if ( $_POST['payload'] ) {
 putenv('PATH=/usr/local/bin');
-echo shell_exec("cd /home/bitrends/web/bitrends.pl/public_html && /usr/bin/git pull 2>&1");
+echo shell_exec("cd /home/ktransform/web/kongrestransformacji.pl/public_html && /usr/bin/git pull 2>&1");
 echo shell_exec('/usr/bin/whoami 2>&1');
 }
 
