fa41050cf79a8df55d25be3cedee886d6482a791
kuba <info@weblead.pl>
2021-10-02 14:17:40 +0200

    first

--- pull.php
+++ pull.php
@@ -6,7 +6,7 @@
 
 if ( $_POST['payload'] ) {
 putenv('PATH=/usr/local/bin');
-echo shell_exec("cd /home/ktransform/web/kongrestransformacji.pl/public_html && /usr/bin/git pull 2>&1");
+echo shell_exec("cd /home/ktransform/web/kongrestransformacji.pl/public_html && /usr/bin/git pull origin branch4 2>&1");
 echo shell_exec('/usr/bin/whoami 2>&1');
 }
 
